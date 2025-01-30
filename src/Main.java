6a74a596cbf63e38775d48b36ef8d21c0cfc8172
shpk93 <shpk930924@gmail.com>
2025-01-31 01:15:07 +0900

    템플릿

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    static int N;
+    static int M;
+
+    public static void main(String[] arg) {
+
     }
 }
