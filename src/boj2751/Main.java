eea6659ec1ea7bacd9be00ed054ee872618792da
shpk93 <shpk930924@gmail.com>
2025-01-30 23:50:06 +0900

    2751

---
+++ src/boj2751/Main.java
@@ -0,0 +1,27 @@
+package boj2751;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    static int N;
+    static int[] arr;
+
+    public static void main(String[] arg) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i : arr) {
+            sb.append(i).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
